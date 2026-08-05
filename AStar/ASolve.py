f5cca21424a7dedb44c444ae812712c1e52b19c0
WilliamKoglin <207467986+WilliamKoglin@users.noreply.github.com>
2026-08-04 22:22:35 -0500

    Version 1.1
    Updated ASolve to return solvability rule message

--- AStar/ASolve.py
+++ AStar/ASolve.py
@@ -7,3 +7,5 @@ testNode = Node(inputBoard())
 sol = ((1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0))
 if (solvable(testNode.board)):
     print(f'Solution: {AStarSolve(testNode,sol)}')
+else:
+    print("Board Not Solvable")
