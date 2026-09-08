585d94cb100cf2ef4cfa937a6bbc9edf7b9adbdb
WilliamKoglin <207467986+WilliamKoglin@users.noreply.github.com>
2026-09-08 15:42:30 -0500

    saving pre-cleanup version

--- AStar/ASolve.py
+++ AStar/ASolve.py
@@ -2,13 +2,13 @@ from AInput import inputBoard
 from ASolvable import solvable
 from ANode import Node
 from AStarAlgo import AStarSolve
-
+from AStarAlgo import IDASolve
 
 def main():
     testNode = Node((1,2,3,4,5,6,7,8,9,10,11,12,0,13,14,15))
     sol = ((1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0))
     if (solvable(testNode.board)):
-        print(f'Solution: {AStarSolve(testNode,sol)}')
+        print(f'Solution: {IDASolve(testNode,sol)}')
     else:
         print("Board Not Solvable")
 
--- AStar/AStarAlgo.py
+++ AStar/AStarAlgo.py
@@ -61,18 +61,48 @@ def traceBack(node):
 # IDA*
 def IDASolve(init,sol):
     #set flimit to initial node's fcost
+    flimit = init.fCost
     #while true
+    while True:
     # set var to IDFS(node, flimit)
+        var = IDFS(init,flimit)
+        if type(var) == list:
+            return var
+        else:
+            flimit = var
+            
     # if ver returns a board, return board
     # else, set flimit to var
 
 def IDFS(node, threshold):
-    #initialize min as max f-value of a 15 puzzle + 1
+    #initialize thresh_candidate as max f-value of a 15 puzzle + 1
+    thresh_candidate = 1000 #reasonable max f-cost
     #if node == sol, call traceback with node, return list
+    if node.board == (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0):
+        return traceBack(node)
     #else cycle through getneighbors(node.board), we will call each node item
-    #if item.fcost < threshold, check if < min idfs(item,threshold), if so, set min to that val
-    #if item.fcost > threshold, check min against item.fcost
-    #return min
+    else:
+        for item in IDNeighbors(node):
+            if item.fCost <= threshold:
+                tval = IDFS(item,threshold)
+                if type(tval) == list:
+                    return tval
+                if tval < thresh_candidate:
+                    thresh_candidate = tval
+            else:
+                if thresh_candidate > item.fCost:
+                    thresh_candidate = item.fCost
+        return thresh_candidate
+    #if item.fcost < threshold, check if < thresh_candidate idfs(item,threshold), if so, set thresh_candidate to that val
+    #if item.fcost > threshold, check thresh_candidate against item.fcost
+    #return thresh_candidate
     
-def getneighbiors(node):
-   #return list of Nodes with boards one move away from away from node excluding pervious move.
+def IDNeighbors(node):
+    #return list of Nodes with boards one move away from away from node excluding pervious move.
+    nodelist = []
+    for item in (getNeighbors(node.board)):
+        if item[1] == node.prev:
+            continue
+        else:
+            nodelist.append(Node(item[0],node,item[1]))
+    return nodelist
